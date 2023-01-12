3d6944721b87e08770452884f463b01a9a001471
Kenneth Presedo <kennethpresedo@live.com>
2023-01-12 15:56:40 -0500

    espresso init

--- controllers/api/users.js
+++ controllers/api/users.js
@@ -1,70 +1,3 @@
-// // /controllers/api/users.js
-
-// const User = require('../../models/user')
-// const jwt = require('jsonwebtoken')
-// const bcrypt = require('bcrypt')
-
-// const checkToken = (req, res) => {
-//   console.log('req.user', req.user)
-//   res.json(req.exp)
-// }
-
-// const dataController = {
-//   async create (req, res, next) {
-//     try {
-//       const user = await User.create(req.body)
-//       console.log(req.body)
-//       // token will be a string
-//       const token = createJWT(user)
-//       // send back the token as a string
-//       // which we need to account for
-//       // in the client
-//       res.locals.data.user = user
-//       res.locals.data.token = token
-//       next()
-//     } catch (e) {
-//       console.log('you got a database problem')
-//       res.status(400).json(e)
-//     }
-//   },
-//   async login (req, res, next) {
-//     try {
-//       const user = await User.findOne({ email: req.body.email })
-//       if (!user) throw new Error()
-//       const match = await bcrypt.compare(req.body.password, user.password)
-//       if (!match) throw new Error()
-//       res.locals.data.user = user
-//       res.locals.data.token = createJWT(user)
-//       next()
-//     } catch {
-//       res.status(400).json('Bad Credentials')
-//     }
-//   }
-// }
-
-// const apiController = {
-//   auth (req, res) {
-//     res.json(res.locals.data.token)
-//   }
-// }
-
-// module.exports = {
-//   checkToken,
-//   dataController,
-//   apiController
-// }
-
-// /* -- Helper Functions -- */
-
-// function createJWT (user) {
-//   return jwt.sign(
-//     // data payload
-//     { user },
-//     process.env.SECRET,
-//     { expiresIn: '24h' }
-//   )
-// }
-
 // /controllers/api/users.js
 
 const User = require('../../models/user')
@@ -80,6 +13,7 @@ const dataController = {
   async create (req, res, next) {
     try {
       const user = await User.create(req.body)
+      console.log(req.body)
       // token will be a string
       const token = createJWT(user)
       // send back the token as a string
@@ -89,6 +23,7 @@ const dataController = {
       res.locals.data.token = token
       next()
     } catch (e) {
+      console.log('you got a database problem')
       res.status(400).json(e)
     }
   },
@@ -119,7 +54,6 @@ module.exports = {
   apiController
 }
 
-/* -- Helper Functions -- */
 
 function createJWT (user) {
   return jwt.sign(
--- src/components/OrderDetail/OrderDetail.js
+++ src/components/OrderDetail/OrderDetail.js
@@ -49,3 +49,4 @@ export default function OrderDetail({ order, handleChangeQty, handleCheckout }) 
       </div>
     );
   }
+  
